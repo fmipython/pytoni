46e206eaeee92b86df0560482fd564315af747a9
Lyuboslav Karev <lyuboslav.karev@sitegroun.com>
2026-06-23 17:03:23 +0300

    fix typo in tool name

--- src/pytoni/tools/__init__.py
+++ src/pytoni/tools/__init__.py
@@ -1,4 +1,4 @@
 from pytoni.tools.fetch_readme import fetch_readme
-from pytoni.tools.get_calendar import get_calendar
+from pytoni.tools.course_timeline import get_calendar
 
 __all__ = ["fetch_readme", "get_calendar"]
