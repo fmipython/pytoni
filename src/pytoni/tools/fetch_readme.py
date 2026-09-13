1ead34a9c17c4e60f591f0aeee62d10f74db30d5
Lyuboslav Karev <lyubo.karev@gmail.com>
2026-09-13 13:27:42 +0300

    Fetch README.md from GitHub instead of local file

--- src/pytoni/tools/fetch_readme.py
+++ src/pytoni/tools/fetch_readme.py
@@ -1,3 +1,10 @@
+import requests
+
+
 def fetch_readme() -> str:
-    with open("COURSE_README.md", "r") as f:
-        return f.read()
+    response = requests.get("https://raw.githubusercontent.com/fmipython/PythonCourse2026/refs/heads/main/README.md")
+
+    if not response:
+        return "Failed to fetch README.md from GitHub."
+
+    return response.text
